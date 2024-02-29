48ac01f073f48f77f8fe88fa0beaaa51c6cea83d
Rubeno Dechua <rdechua@dons.usfca.edu>
2024-02-28 22:13:59 -0800

    ShakerSort 1: Passed the test, but still yet to be tested with the string builder, not checked to be within the style guidelines, no documentations, and still has some failed attempt codes.

--- src/main/java/sortingSongCollection/sortingAlgorithms/ShakerSort.java
+++ src/main/java/sortingSongCollection/sortingAlgorithms/ShakerSort.java
@@ -29,7 +29,128 @@ public class ShakerSort implements SortingAlgorithm {
      */
     //@Override
     public void sort(Song[] songs, int low, int high, boolean isAscending, StringBuilder sb) {
-            // FILL IN CODE
+        // FILL IN CODE
+        /*if(low+1 < high){
+            for(int i = 0; i < songs.length; i++){
+                System.out.print(songs[i].getTitle());
+            }
+            System.out.println("");
+
+            Song min = songs[low];
+            Song max = songs[low];
+            int minindex = 0;
+            int maxindex = 0;
+            for(int i = low; i <= high; i++){
+                if(songs[i].compareTo(min) < 0){
+                    min = songs[i];
+                    minindex = i;
+                }else if(songs[i].compareTo(max) > 0){
+                    max = songs[i];
+                    maxindex = i;
+                }
+            }
+            for(int i = 0; i < low; i++){
+                System.out.print(" ");
+            }
+            System.out.println("^ low " + low);
+            for(int i = 0; i < high; i++){
+                System.out.print(" ");
+            }
+            System.out.println("^ high " + high);
+
+
+            System.out.println(min.getTitle() + " " + minindex);
+            System.out.println(max.getTitle() + " " + maxindex);
+
+            if(isAscending) {
+                for (int j = minindex; j > low; j--) {
+                    songs[j] = songs[j - 1];
+                }
+                songs[low] = min;
+                for (int k = maxindex; k < high; k++) {
+                    songs[k] = songs[k + 1];
+                }
+                songs[high] = max;
+            }else{
+                for (int j = maxindex; j > low; j--) {
+                    songs[j] = songs[j - 1];
+                }
+                songs[low] = max;
+                for (int k = minindex; k < high; k++) {
+                    songs[k] = songs[k + 1];
+                }
+                songs[high] = min;
+            }
+            for(int i = 0; i < songs.length; i++){
+                System.out.print(songs[i].getTitle());
+            }
+            System.out.println("");
+            System.out.println("END");
+
+            sort(songs, low+1, high-1, isAscending, sb);
+        }
+
+         */
+        boolean check = true;
+        while(check){
+
+            for(int i = low; i < high; i++){
+                System.out.print(songs[i].getTitle());
+            }
+            System.out.println(" START");
+
+            check = false;
+            for (int i = high; i > low; i--) {
+                if ((songs[i].compareTo(songs[i-1]) < 0 && isAscending) ||
+                        (songs[i].compareTo(songs[i-1]) > 0 && !isAscending)) {
+                    Song tmp = songs[i];
+                    songs[i] = songs[i-1];
+                    songs[i-1] = tmp;
+                    check = true;
+                }
+            }
+
+            for(int i = low; i < high; i++){
+                System.out.print(songs[i].getTitle());
+            }
+            System.out.println(" Small");
+
+
+            low++;
+            if(!check){
+                break;
+            }
+            check = false;
+
+            for (int i = low; i < high; i++) {
+                if ((songs[i].compareTo(songs[i+1]) > 0 && isAscending) ||
+                (songs[i].compareTo(songs[i+1]) < 0 && !isAscending)) {
+                    Song tmp = songs[i];
+                    songs[i] = songs[i+1];
+                    songs[i+1] = tmp;
+                    check = true;
+                }
+            }
+            for(int i = low; i < high; i++){
+                System.out.print(songs[i].getTitle());
+            }
+            System.out.println(" Big");
+            high--;
+
+
+            /*
+            System.out.println("");
+            for(int i = 0; i < low; i++){
+                System.out.print(" ");
+            }
+            System.out.println("^ low " + low);
+            for(int i = 0; i < high; i++){
+                System.out.print(" ");
+            }
+            System.out.println("^ high " + high);
+            */
+        }
+
 
     }
 }
